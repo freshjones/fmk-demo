fb65bc2fe6c9c0d8c7b5aa7ca32f75347e7d3f2e
William Jones <billy@freshjones.com>
2012-11-11 17:20:49 -0500

    full theming completed

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>FiberMark Inc.</title>
 <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/featured.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/spotlights.css" type="text/css" media="screen" />
 
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
@@ -12,6 +14,8 @@
 <script type="text/javascript" src="scripts/bus_seg.js"></script>
 </head>
 <body class="luxury">
+
+
 <div class="center">
 	<div class="inner bottom-rule">
 	
@@ -19,22 +23,56 @@
 </div>
 <div class="left">
 	<div class="inner bottom-rule">
-		<div class="content">
+		
+		<div class="top pane-content">
 			<div class="logo">
-				<a href="/"><img src="images/logo-mask.png" /></a>
+				<a href="/"><img src="images/logo-mask.png" alt="FiberMark, Inc." /></a>
 			</div>
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/">Products</a></li>
-				<li><a href="/">Design</a></li>
-				<li><a href="/">Environment</a></li>
-				<li><a href="/">Faces</a></li>
-				<li><a href="/">News</a></li>
-				<li><a href="/">About</a></li>
-				<li><a href="/">Contact</a></li>
-				<li><a href="/">Cart</a></li>
-			</ul>
+			<div class="">
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/">Products</a></li>
+					<li><a href="/">Design</a></li>
+					<li><a href="/">Environment</a></li>
+					<li><a href="/">Faces</a></li>
+					<li><a href="/">News</a></li>
+					<li><a href="/">About</a></li>
+					<li><a href="/">Contact</a></li>
+					<li><a href="/">Cart</a></li>
+					<li class="search">
+						<input type="text" class="form-text" maxlength="128" size="15" value="Search..." name="search_block_form" id="edit-search-block-form--2" onfocus="if (this.value == 'Search...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search...';}" title="Enter the terms you wish to search for.">
+					</li>
+				</ul>
+			</div>
 		</div>
+		
+		<div class="bottom pane-content">
+			
+			<div class="social-media">
+				<ul>
+					<li class="first"><a target="_blank" class="youtube" href="http://www.youtube.com/user/FriendsOfFibermark"><img src="images/youtube.png" alt="YouTube" /></a></li>
+					<li><a target="_blank" class="twitter" href="http://twitter.com/fibermark"><img src="images/twitter.png" alt="Twitter" /></a></li>
+					<li><a target="_blank" class="linkedin" href="http://www.linkedin.com/companies/fibermark"><img src="images/linkedin.png" alt="LinkedIn" /></a></li>
+					<li class="last"><a target="_blank" class="facebook" href="http://www.facebook.com/pages/FiberMark-North-America-Inc/339651484924?ref=ts"><img src="images/facebook.png" alt="FaceBook" /></a></li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+			<div class="">
+    			<ul class="menu">
+    				<li class="first leaf"><a title="" href="/fibermark-careers-be-part-innovation">Career</a></li>
+					<li class="leaf"><a title="" href="/terms-use">Legal</a></li>
+					<li class="leaf"><a title="" href="/privacy-policy">Privacy</a></li>
+					<li class="translate last leaf"><a title="" href="/translate">Translate</a></li>
+				</ul>  
+			</div>
+
+		</div>
+		
+		<div class="copyright">
+			&copy; 2012
+		</div>
+		
+	
 	</div>
 </div>
 <div class="right">
@@ -48,69 +86,154 @@
 		<tr>
 			<td>
 			
-				<table class="control-table">
+				<table class="wrapper">
 					<tr>
-						<td class="control-cell first">
-						
-							<table class="wrapper">
-								<tr>
-									<td class="control-cell-inner control-prev"><a href="/" class="control-button"><span>Prev</span></a></td>
-								</tr>
-							</table>
-				
-						</td>
-						<td class="control-cell">
-							<table class="wrapper">
-								<tr>
-									<td class="control-cell-inner control-content">
-										<div class="control-content-inner">
-											<span class="heading">Luxury Packaging</span>
-											<span class="subheading">Put FiberMark on the SURFACE, to accentuate the quality of what's INSIDE.</span>
+						<td class="top-spotlights">
+							
+							<div class="featured-wrapper">
+								<div class="featured-wrapper-inner">
+								
+									<div class="featured-content-wrapper">
+										<div class="featured-content-wrapper-inner">
+											
+											<div class="featured-content-background" >
+												<img src="images/bg_featured-product-info.png" alt="Featured Product" />
+											</div>
+											
+											<div class="featured-content">
+												<div class="subhead">Featuring</div>
+												<div class="head">Iridescents by Corvon<sup>&reg;</sup></div>
+												<ul class="menu">
+													<li class="first"><a href="/">Overview</a></li>
+													<li><a href="/">Virtual Design</a></li>
+													<li class="last"><a href="/">Showcase</a></li>
+												</ul>
+											</div>
+											
+											<div class="featured-icon" >
+												<img src="images/luxury-icon.png" alt="Featured Luxury Stock" />
+											</div>
+											
 										</div>
+									</div>
+										
+									<div class="featured-image">
+										<div class="featured-image-inner"><img src="images/featured-luxury.png" alt="Featured Luxury Stock"  /></div>
+									</div>
+								</div>
+							</div>
+							
+							<table class="control-table">
+								<tr>
+									<td class="control-cell first">
+									
+										<table class="wrapper">
+											<tr>
+												<td class="control-cell-inner control-prev"><a href="/" class="control-button"><span>Prev</span></a></td>
+											</tr>
+										</table>
+							
+									</td>
+									<td class="control-cell">
+										<table class="wrapper">
+											<tr>
+												<td class="control-cell-inner control-content">
+													<div class="control-content-inner">
+														<span class="heading">Luxury Packaging</span>
+														<span class="subheading">Put FiberMark on the SURFACE, to accentuate the quality of what's INSIDE.</span>
+													</div>
+												</td>
+											</tr>
+										</table>
+									</td>
+									<td class="control-cell">
+							
+										<table class="wrapper">
+											<tr>
+												<td class="control-cell-inner">&nbsp;</td>
+											</tr>
+										</table>
+									</td>
+									<td class="control-cell last">
+										<table class="wrapper">
+											<tr>
+												<td class="control-cell-inner control-next"><a href="/" class="control-button"><span>Next</span></a></td>
+											</tr>
+										</table>
 									</td>
 								</tr>
 							</table>
-						</td>
-						<td class="control-cell">
-							<table class="wrapper">
-								<tr>
-									<td class="control-cell-inner">&nbsp;</td>
-								</tr>
-							</table>
-						</td>
-						<td class="control-cell last">
-							<table class="wrapper">
-								<tr>
-									<td class="control-cell-inner control-next"><a href="/" class="control-button"><span>Next</span></a></td>
-								</tr>
-							</table>
+							
+							
+									
 						</td>
 					</tr>
+					<tr>
+						<td class="bottom-spotlights">
+							
+							<div class="spotlights">
+								
+								<div class="spotlights-inner">
+								
+									<div class="spotlight">
+										<div class="spotlight-image"><img src="images/spotlight-1.png" alt="Spotlight One" /></div>
+										<div class="spotlight-content">
+											<div class="subhead">One-Of-A-Kind</div>
+											<div class="head"><a href="/">Custom Branding</a></div>
+										</div>
+									</div>
+								
+									<div class="spotlight">
+										<div class="spotlight-image"><img src="images/spotlight-1.png" alt="Spotlight Two" /></div>
+										<div class="spotlight-content">
+											<div class="subhead">Announcing</div>
+											<div class="head"><a href="/">Award Winners</a></div>
+										</div>
+									</div>
+								
+									<div class="spotlight">
+										<div class="spotlight-image"><img src="images/spotlight-1.png" alt="Spotlight Three" /></div>
+										<div class="spotlight-content">
+											<div class="subhead">Inspiration</div>
+											<div class="head"><a href="/">Product Showcase</a></div>
+										</div>
+									</div>
+								
+								</div>
+								
+							</div>	
+							
+							
+						</td>
+					</tr>
+					
 				</table>
-
+	
 			</td>
 		</tr>
 		<tr>
 			<td class="bottom-controls">
-					<div>
-						<ul>
-							<li><a href="/">Covering Solutions</a></li>
-							<li><a href="/">Graphic Design</a></li>
-							<li class="active"><a href="/">Luxury Packaging</a></li>
-							<li><a href="/">Performance Boards</a></li>
-							<li><a href="/">Print Media</a></li>
-							<li><a href="/">Security</a></li>
-							<li><a href="/">Technical Specialties</a></li>
-							<li><a href="/">Technical Textiles</a></li>
-						</ul>
-					</div>
-				
+				<div>
+					<ul>
+						<li><a href="/">Covering Solutions</a></li>
+						<li><a href="/">Graphic Design</a></li>
+						<li class="active"><a href="/">Luxury Packaging</a></li>
+						<li><a href="/">Performance Boards</a></li>
+						<li><a href="/">Print Media</a></li>
+						<li><a href="/">Security</a></li>
+						<li><a href="/">Technical Specialties</a></li>
+						<li><a href="/">Technical Textiles</a></li>
+					</ul>
+				</div>
 			</td>
 		</tr>
 	</table>
+	
 		
 	
 </div>
+
+
 
 </body>
 </html>
